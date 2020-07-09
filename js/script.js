be98f70ec6e967b5854b5d0991a94f10e9eae5f5
Numan Ahmed <55399746+FreelancerNuman@users.noreply.github.com>
2020-07-09 15:41:54 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-/* navbar js */
-$(window).scroll(function(){
-	var positonTop =$(document).scrollTop();
-		
-	if((positonTop > 20)){
-		$('.navbar').addClass('nav-color');
-	}
-	if((positonTop < 20)){
-		$('.navbar').removeClass('nav-color');
-	}
-		
-});
 /* Slider js */
 $('.carousel').carousel({
   interval: 2000
