962d8fd58bb52d8505d117e2659423f507d738eb
Numan Ahmed <55399746+FreelancerNuman@users.noreply.github.com>
2020-07-11 03:45:29 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,23 @@
 $('.carousel').carousel({
   interval: 2000
 })
-	
+/* Statistic Conunter */
+var counters = document.querySelectorAll('.number');
+var speed = 200;
+
+counters.forEach(counter => {
+	function updateCount(){
+		var target = +counter.getAttribute('data-target');
+		var count = +counter.innerText;
+		
+		var inc = target / speed;
+		
+		if(count < target){
+			counter.innerText = Math.ceil(count + inc);
+			setTimeout(updateCount,1 );
+		}else{
+			count.innerText = target;
+		}
+	}
+	updateCount(); 
+});
